2f6f936b41878b2141a2e5af09593a017015764a
homnath <hngharti@gmail.com>
2020-07-17 14:14:12 -0400

    - minor

--- src/specfem3d.F90
+++ src/specfem3d.F90
@@ -1,5 +1,4 @@
 !===============================specfem3d.F90===================================
-! include 'license.txt'
 ! REVISION:
 !   HNG, Aug 25,2011; HNG, Jul 14,2011; HNG, Jul 11,2011; Apr 09,2010
 subroutine specfem3d()
