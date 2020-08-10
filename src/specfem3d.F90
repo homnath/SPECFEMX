a9dfc2ad209f205c2db65c13cd8f6f0f54aa7dc0
homnath <hngharti@gmail.com>
2020-08-09 20:46:45 -0400

    - minor

--- src/specfem3d.F90
+++ src/specfem3d.F90
@@ -3,7 +3,7 @@
 !   HNG, Aug 25,2011; HNG, Jul 14,2011; HNG, Jul 11,2011; Apr 09,2010
 !-------------------------------------------------------------------------------
 subroutine specfem3d()
-! Import necessary libraries
+! Import necessary modules
 use dimensionless
 use global
 use string_library, only : parse_file
