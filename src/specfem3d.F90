b1b7d5286d62b5ac2b6583ea06f1687f01cda6a9
homnath <hngharti@gmail.com>
2022-12-13 11:48:23 -0500

    - cleanup

--- src/specfem3d.F90
+++ src/specfem3d.F90
@@ -842,19 +842,13 @@ loop_step: do i_step=istep0,nstep
       flush(logunit)
     endif
     call apply_mtraction(extload,errcode,errtag)
-    if(myrank==0)print*,'bp000'
-    !call sync_process
     call control_error(errcode,errtag,stdout,myrank)
     if(myrank==0)then
       write(logunit,*)'complete!',maxval(abs(extload))
       flush(logunit)
     endif
-    print*,myrank,'bp00'
   endif
   
-  call sync_process
-  if(myrank==0)print*,'bp0'
- 
   ! compute load contributed by the earthquake slip
   ! split-node apparoch: prescribe the slip on the fault explicitly
   if(iseqsource.and.eqsource_type.eq.3)then
@@ -903,8 +897,6 @@ loop_step: do i_step=istep0,nstep
     endif
   endif
   
-  call sync_process
-  if(myrank==0)print*,'bp2'
   ! Modify RHS vector for prescribed displacements
   ! WARNING: need to check for nedofu
   do i_elmt=1,nelmt
@@ -924,10 +916,6 @@ loop_step: do i_step=istep0,nstep
     enddo
   enddo ! i_elmt
 
-  call sync_process
-  if(myrank==0)print*,'bp3'
-  if(myrank==0)print*,solver_type,builtin_solver
-  
   if(solver_type.eq.builtin_solver)then
     ! Compute diagonal precoditioner
     dprecon=ZERO
@@ -987,9 +975,6 @@ loop_step: do i_step=istep0,nstep
     endif
   endif !(solver_type.eq.builtin_solver)
 
-  call sync_process
-  if(myrank==0)print*,'bp4'
-  
   extload(0)=ZERO
   ! set BC nodal displacements to nodalu array
   if(ISDISP_DOF)then
@@ -1000,9 +985,6 @@ loop_step: do i_step=istep0,nstep
       enddo
     enddo
   endif
-  
-  call sync_process
-  if(myrank==0)print*,'bp5'
   
   ! set BC nodal potential to nodalphi array
   if(ISPOT_DOF)then
@@ -1015,8 +997,6 @@ loop_step: do i_step=istep0,nstep
   endif
   ksp_tot=0; nl_iter=0
 
-  call sync_process
-  if(myrank==0)print*,'bp6'
   ! only for fault
   load=selfload+extload+ubcload+rhoload
 
