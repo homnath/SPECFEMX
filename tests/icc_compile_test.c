4251227fc55ee24e97e99f7fa2023a6db72ee464
Will Eaton <we3822@princeton.edu>
2023-01-27 14:25:06 -0500

    update actions build

--- tests/icc_compile_test.c
+++ tests/icc_compile_test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Printing Hello, World! with icc");
+   printf("Printing Hello, World! with icc\");
    return 0;
 }
