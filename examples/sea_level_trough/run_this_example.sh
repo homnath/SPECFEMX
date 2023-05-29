613b693e11c3cbe9a5debbdd943eb18e9c22f9e1
Will Eaton <we3822@princeton.edu>
2023-05-29 14:39:10 -0400

    Add partmesh test

--- examples/sea_level_trough/run_this_example.sh
+++ examples/sea_level_trough/run_this_example.sh
@@ -8,20 +8,30 @@ echo "  Starting sea level trough example"
 echo ""
 
 
-# Copy and rename: 
-cp -r examples/sea_level_trough/input_files ./build/input
-mv ./build/input/input_files ./build/input/uptrough
+example_path="./examples/sea_level_trough/input_files"
+inp_path="input/uptrough"
+build_inp_path="./build/${inp_path}"
 
-echo "-  Copied input files to build/input directory & renamed"
+# Copy and rename: 
+cp -r $example_path $build_inp_path
+echo "-  Copied input files to ${build_inp_path}" 
 
 
 # move into build directory and copy psem to build
-cd build && cp input/uptrough/uptrough.slurm ./ 
+cd build && cp "${inp_path}/uptrough.slurm" ./ 
 echo "-  Copied slurm file to build directory"
 echo "-  Running partmesh: "
 
 
-# Run the mesh partitioning for 40 nodes
-./bin/partmesh input/uptrough/uptrough.psem 
 
+# Make the output_uptrough dir: 
+mkdir output_uptrough 
+mkdir tmp 
+
+
+# Run the mesh partitioning for 40 nodes
+./bin/partmesh "${inp_path}/uptrough.psem" > partmesh_output.txt  
+
+# Co
+cmp ../examples/sea_level_trough/stable_partmesh_output.txt partmesh_output.txt
 
