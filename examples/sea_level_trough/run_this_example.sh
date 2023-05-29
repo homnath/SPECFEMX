9f667ca4259afc1613930885e5a50647cfd80204
Will Eaton <we3822@princeton.edu>
2023-05-29 13:51:13 -0400

    Update SL example runthisex

--- examples/sea_level_trough/run_this_example.sh
+++ examples/sea_level_trough/run_this_example.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+echo ""
+echo ""
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "Finished compiling SPECFEMX: "
+echo "  Starting sea level trough example"
+echo ""
 
-echo "Starting sea level trough example"
 
 # Copy and rename: 
 cp -r examples/sea_level_trough/input_files ./build/input
 mv ./build/input/input_files ./build/input/uptrough
 
+echo "-  Copied input files to build/input directory & renamed"
+
+
 # move into build directory and copy psem to build
 cd build && cp input/uptrough/uptrough.slurm ./ 
+echo "-  Copied slurm file to build directory"
+echo "-  Running partmesh: "
+
 
 # Run the mesh partitioning for 40 nodes
 ./bin/partmesh input/uptrough/uptrough.psem 
+
+
