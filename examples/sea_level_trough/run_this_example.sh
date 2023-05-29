7079df27882860a39df6b8b6a2a7624a13dcd7f8
Will Eaton <we3822@princeton.edu>
2023-05-29 13:43:33 -0400

    Update SL example

--- examples/sea_level_trough/run_this_example.sh
+++ examples/sea_level_trough/run_this_example.sh
@@ -4,5 +4,11 @@
 echo "Starting sea level trough example"
 
 # Copy and rename: 
-cp -r examples/sea_level_trough/input_files ./input
-mv ./input/input_files ./input/uptrough
+cp -r examples/sea_level_trough/input_files ./build/input
+mv ./build/input/input_files ./build/input/uptrough
+
+# move into build directory and copy psem to build
+cd build && cp input/uptrough/uptrough.slurm ./ 
+
+# Run the mesh partitioning for 40 nodes
+./bin/partmesh input/uptrough/uptrough.psem 
